e03c8689809f2eeeaceb53a1773202831a45b891
Saroj Sahoo <44677@corp.hexaware.com>
2019-03-27 11:41:13 +0530

    added java program

---
+++ ArrayListDefaultsort.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ArrayListDefaultsort {
+
+	public static void main(String[] args) {
+		List<String> ss = new ArrayList<String>();
+		ss.add("saroj");
+		ss.add("Bapuni");
+		ss.add("Jiya");
+		ss.add("lubun");
+		ss.add("Mohit");
+		Collections.sort(ss);
+		System.out.println(ss);
+		
+		// customized sorting order using inner class
+		/*Collections.sort(ss, new Comparator<String>() {
+
+			@Override
+			public int compare(String arg0, String arg1) {
+				return -arg0.compareTo(arg1);
+			}
+			
+		});
+		System.out.println(ss);*/
+		// customized sorting order using lambda
+		Collections.sort(ss, (p1,p2)-> -p1.compareTo(p2));
+		System.out.println(ss);
+		
+	}
+
+}
